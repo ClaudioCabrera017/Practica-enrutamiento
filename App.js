29223f06261d1c6aad353f0a41c38d713ab89820
ClaudioCabrera017 <118058713+ClaudioCabrera017@users.noreply.github.com>
2023-01-28 09:08:00 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { BrowserRouter, Link, Routes,Route } from 'react-router-dom';
+import Casa from './Casa';
+import Numero from './Numero';
+import Hola from './Hola';
+import Holacolor from './Holacolor';
+
+function App() {
+  return (
+    <BrowserRouter>
+      
+      <Link to="/home">1. Home</Link>{" | "}
+      <Link to="/number/1">2. Number</Link>{" | "}
+      <Link to="/hello">3. Hello</Link>{" | "}
+      <Link to="/hello/blue/red">4. Hello Color</Link>
+
+      <Routes>
+        <Route path="/home"><Casa /></Route>
+        <Route path="/number/:number" render={(routeProps) => <Numero {...routeProps} />} />
+        <Route path="/:hello/:color/:background" render={(routeProps) => <Hola {...routeProps} />} />
+        <Route path="/:hello" render={(routeProps) => <Holacolor {...routeProps} />} />
+      </Routes>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
