ec81eea650c192a5e45890821b203c6575c77934
ClaudioCabrera017 <118058713+ClaudioCabrera017@users.noreply.github.com>
2023-01-30 08:51:47 -0300

    Update Holacolor.jsx

--- Holacolor.jsx
+++ Holacolor.jsx
@@ -1,8 +1,19 @@
 import React from "react";
-
+import { useParams } from 'react-router'
 const Holacolor = () => {
-  return (
-    <h3 style={{backgroundColor: "red"}}>The word is: </h3>
-  )
-}
+  const {backgroundColor, color, message} = useParams();
+    const isColor = (color) => {
+        let style = new Option().style;
+        style.color = color;
+        return style.color === color;
+    }
+    const getStyle = (properties) => {
+        return {
+            backgroundColor: isColor(properties.backgroundColor) ? properties.backgroundColor : 'red',
+            color: isColor(properties.color) ? properties.color : 'blue'
+        };
+    }
+    return (
+        <div style={getStyle({backgroundColor: backgroundColor, color: color})}>{`${message}`}</div>
+    )
 export default Holacolor;
